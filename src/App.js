05bbb88d65716bf92f98eb77a26a1e5cf61d8c24
Tarekul1995 <islamtarek4@gmail.com>
2019-08-19 16:36:06 +0600

    autoComplete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+import { ListGroup,Container,Row,Col,FormControl } from "react-bootstrap";
+import Downshift from 'downshift'
+import matchSorter from 'match-sorter'
+import axios from "axios";
+
 
 function App() {
+
+  const [users,setUser] = useState([]);
+  const getMatchUser = value => value ? matchSorter(users,value,{keys:['name','username']}) : users
+
+  useEffect(()=>{
+    const fetchUser = async () =>{
+      const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setUser(res.data)
+    }
+    fetchUser()
+  },[])  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container>
+    <Row>
+      <Col></Col>
+      <Col>
+      <Downshift itemToString={(item)=>item ? item.name : ''}>
+        {
+          ({getInputProps,highlightedIndex,getMenuProps,isOpen,getItemProps,inputValue})=>(
+            <div>
+              <FormControl {...getInputProps()} type="text" placeholder="Search" className="mr-sm-2 mt-5" />
+              <ListGroup {...getMenuProps({
+                style:{
+                  maxHeight:200,
+                  overflowY:'scroll'
+                }
+              })}>
+                {
+                  isOpen ? getMatchUser(inputValue).map((user,index)=>(
+                    <ListGroup.Item {...getItemProps({
+                      item:user,key:user.id,style:{
+                        backgroundColor:
+                          index === highlightedIndex ? '#6682E7' : null,
+                      },
+                    })}>{user.name}({user.username})</ListGroup.Item>
+                  )) : null
+                }
+              </ListGroup>
+            </div>
+          )
+        }
+      </Downshift>
+      </Col>
+      <Col></Col>
+    </Row>
+  </Container>
   );
 }
 
